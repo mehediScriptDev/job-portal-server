6f28f75ddf27399022eb0f149e93b9bfbbb0d04b
mehediScriptDev <mehedi25122003@gmail.com>
2025-08-20 23:37:52 +0600

    mongodb setup almost done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
@@ -9,6 +11,33 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send('job is fallen from the sky');
 })
+
+
+
+const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port,()=>{
     console.log('pinged');
