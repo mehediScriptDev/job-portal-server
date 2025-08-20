a14dca477535e1eab4cf2d7e57f904306dd53092
mehediScriptDev <mehedi25122003@gmail.com>
2025-08-21 00:10:41 +0600

    connected to mongodb with user n pass

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/',(req,res)=>{
 
 
 
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,9 +32,15 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
