715a2b255984cf6446eda79a21b3b6d43a8715e1
mehediScriptDev <mehedi25122003@gmail.com>
2025-08-20 22:42:33 +0600

    backend setup and nodemon error fixed

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('job is fallen from the sky');
+})
+
+app.listen(port,()=>{
+    console.log('pinged');
+})
