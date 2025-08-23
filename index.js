fcb8f0f861e6c255ce0431f6b2d69ad9b74448ca
mehediScriptDev <mehedi25122003@gmail.com>
2025-08-23 16:13:42 +0600

    dynamic id routes done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -40,7 +40,13 @@ async function run() {
       const cursor = jobCollections.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
 
+    app.get('/jobs/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobCollections.findOne(query);
+      res.send(result)
     })
 
 
