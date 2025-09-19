6d79fe6449ffbcf2ac51c4aa3888e8ec0b74d25a
Mehedi <mehedi25122003@gmail.com>
2025-09-19 17:19:06 +0600

    i guess its fixed

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("job is fallen from the sky");
+});
 
-app.get('/',(req,res)=>{
-    res.send('job is fallen from the sky');
-})
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -32,39 +29,51 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-   
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-    const jobCollections = client.db('chakri').collection('mela');
-    const applicationCollections = client.db('chakri').collection('abedon');
+    const jobCollections = client.db("chakri").collection("mela");
+    const applicationCollections = client.db("chakri").collection("abedon");
 
-    app.get('/jobs', async(req,res)=>{
+    app.get("/jobs", async (req, res) => {
       const cursor = jobCollections.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    
-    app.get('/jobs/:id', async(req,res)=>{
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await jobCollections.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.post('/jobapplications', async(req,res)=>{
+    app.post("/jobapplications", async (req, res) => {
       const application = req.body;
       const result = await applicationCollections.insertOne(application);
       res.send(result);
-    })
+    });
 
-    app.get('/job-application',async(req,res)=>{
+    app.get("/job-application", async (req, res) => {
       const email = req.query.email;
-      const query = {applicant_email: email}
+      const query = { applicant_email: email };
       const result = await applicationCollections.find(query).toArray();
-      res.send(result);
-    })
 
+      // fokkira way
+      for (const application of result) {
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobCollections.findOne(query1);
+        if (job) {
+          application.title = job.title;
+          application.jobType = job.jobType;
+          application.category = job.category;
+          application.company = job.company;
+          application.company_logo = job.company_logo;
+        }
+      }
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,7 +81,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.listen(port,()=>{
-    console.log("pinged");
-})
+app.listen(port, () => {
+  console.log("pinged");
+});
